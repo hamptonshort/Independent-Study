0d0e72c26b81a0d29c9d9493c0c1d1e44d6d94a5
hamptonshort <hamptonshort@gmail.com>
2016-10-21 09:43:22 -0500

    Delete water.c

--- water.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-int main (void)
-{
-    printf("How many minutes do you shower?\n") ;
-    int x = GetInt();
-    printf("You waste %.1f waterbottles a day\n", x * 1.5 * 128 / 16) ;
-    
-}
